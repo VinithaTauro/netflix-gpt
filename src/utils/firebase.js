304d8bb6d18e45e5194d9ea5e8752be9c3a96968
VinithaTauro <vinithatauro18@gmail.com>
2025-07-24 15:36:44 +0530

    firebase configuration

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,13 +9,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAq94P-eakT6Wlv5yzxDVvY9NUVLhRwEJA",
-  authDomain: "netflixgpt-25eb3.firebaseapp.com",
-  projectId: "netflixgpt-25eb3",
-  storageBucket: "netflixgpt-25eb3.firebasestorage.app",
-  messagingSenderId: "56775429172",
-  appId: "1:56775429172:web:4129449eb95c2e5a2d4e61",
-  measurementId: "G-KBKYKH47YC",
+  //config
 };
 
 // Initialize Firebase
